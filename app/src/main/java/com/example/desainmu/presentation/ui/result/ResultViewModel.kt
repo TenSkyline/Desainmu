76b46aa0daed9882af56d3e986567fa4e9adcc03
TenSkyline <rayvaldokncc@gmail.com>
2025-02-27 22:05:27 +0700

    refactor: Remove handling logic from ResultViewModel
    This commit removes the handling logic for `ResultEvent` from `ResultViewModel`.
    
    **Changes:**
    
    *   Commented out the `when` block in `handleEvent` function.
    * Removed the logic to update the `ResultState` according to the received `ResultEvent`.

--- app/src/main/java/com/example/desainmu/presentation/ui/result/ResultViewModel.kt
+++ app/src/main/java/com/example/desainmu/presentation/ui/result/ResultViewModel.kt
@@ -14,16 +14,16 @@ class ResultViewModel : ViewModel() {
     val uiState: StateFlow<ResultState> get() = _uiState.asStateFlow()
 
     fun handleEvent(event: ResultEvent) {
-        when (event) {
-            is ResultEvent.LingkarBadan -> _uiState.update {
-                it.copy(lingkarBadan = event.lingkarBadan)
-            }
-            is ResultEvent.PanjangBaju -> _uiState.update {
-                it.copy(panjangBaju = event.panjangBaju)
-            }
-            is ResultEvent.PanjangSeluruhBahu -> _uiState.update {
-                it.copy(panjangSeluruhBahu = event.panjangSeluruhBahu)
-            }
-        }
+//        when (event) {
+//            is ResultEvent.LingkarBadan -> _uiState.update {
+//                it.copy(lingkarBadan = event.lingkarBadan)
+//            }
+//            is ResultEvent.PanjangBaju -> _uiState.update {
+//                it.copy(panjangBaju = event.panjangBaju)
+//            }
+//            is ResultEvent.PanjangSeluruhBahu -> _uiState.update {
+//                it.copy(panjangSeluruhBahu = event.panjangSeluruhBahu)
+//            }
+//        }
     }
 }
